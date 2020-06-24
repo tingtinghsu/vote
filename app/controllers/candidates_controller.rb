a14a08d49e31cdcd177bc0a57187b99a3b2f282a
Hui-Ting Hsu <tingtinghsu@Hui-TingdeMacBook-Air.local>
2020-06-24 16:42:01 +0800

    udate/destroy candidates

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -17,6 +17,35 @@ class CandidatesController < ApplicationController
         end
     end
 
+    def show
+        @candidate = Candidate.find_by(id: params[:id])
+        if @candidate.nil?
+            render file: "#{Rails.root}/public/404.html", status: 404
+        end
+    end
+
+    def edit
+        @candidate = Candidate.find_by(id: params[:id])  
+    end
+
+    def update
+        @candidate = Candidate.find_by(id: params[:id])
+        if @candidate.update(clean_params)
+            flash[:notice] = "Candidate updated!"
+            redirect_to '/candidates'    
+        else
+            flash[:notice] = "Please fill the blank!"            
+            render :edit
+        end
+    end
+
+    def destroy
+        @candidate = Candidate.find_by(id: params[:id])
+        @candidate.destroy 
+        flash[:notice] = "Candidate destroyed!"
+        redirect_to '/candidates'         
+
+    end
     private
     def clean_params
         params.require(:candidate).permit(:name, :party, :age, :politics)
