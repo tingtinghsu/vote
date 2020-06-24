f30f8fc7f6b6c7ab4745f610083fb4cbedec5845
Hui-Ting Hsu <tingtinghsu@Hui-TingdeMacBook-Air.local>
2020-06-24 12:28:08 +0800

    show candidates table

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,11 +1,25 @@
 class CandidatesController < ApplicationController
     def index
-    
+        @candidates = Candidate.all
     end
     def new
-    
+        @candidate = Candidate.new
     end    
     def create
-    
+        @candidate = Candidate.new(clean_params)
+
+        if @candidate.save
+            flash[:notice] = "Candidate created!"
+            redirect_to '/candidates'
+        else
+            flash[:notice] = "Please fill the blank!"            
+            render :new
+        end
     end
+
+    private
+    def clean_params
+        params.require(:candidate).permit(:name, :party, :age, :politics)
+    end
+
 end
